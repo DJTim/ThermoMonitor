56c5f7b6c04dddaffdaf4399a3506347b035fc16
Tim Reynaert <tim.reynaert@me.com>
2014-07-15 09:21:46 +0200

    Log saving to subdirectory

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import time, signal, sys
+import time, signal, sys, os.path
 
 sys.path.append('Adafruit_ADS1x15')     #otherwise not possible to import lib
 from Adafruit_ADS1x15 import ADS1x15
@@ -11,16 +11,22 @@ gain = 4096
 sps = 250
 
 #AD Init
-adc = ADS1x15(ic=ADS1115)
+adc1 = ADS1x15(address= 0x48, ic=ADS1115)
 
 def readAD():
+	directory = "log"
 	t = time.strftime("%H:%M")
-	filename = time.strftime("%d%m%y%H") + ".csv"
+	filename = directory + "/" + time.strftime("%d%m%y%H") + ".csv"
 	
-	ch1 = adc.readADCSingleEnded(0, gain, sps) / 1000
-	ch2 = adc.readADCSingleEnded(1, gain, sps) / 1000
-	ch3 = adc.readADCSingleEnded(2, gain, sps) / 1000
-	ch4 = adc.readADCSingleEnded(3, gain, sps) / 1000
+	try:				#check directory existance
+		os.mkdir(directory)
+	except Exception:
+		pass
+
+	ch1 = adc1.readADCSingleEnded(0, gain, sps) / 1000
+	ch2 = adc1.readADCSingleEnded(1, gain, sps) / 1000
+	ch3 = adc1.readADCSingleEnded(2, gain, sps) / 1000
+	ch4 = adc1.readADCSingleEnded(3, gain, sps) / 1000
 	
 	out = "%s,%.6f,%.6f,%.6f,%.6f" % (t, ch1, ch2, ch3, ch4)	
 
